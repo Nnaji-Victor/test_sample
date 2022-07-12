1c1b8d8d7efe4d02b0b1be781adab10e9f597c2a
Nnaji V <nnajivictorr@gmail.com>
2022-07-12 15:54:57 +0100

    updated submit function

--- src/App.js
+++ src/App.js
@@ -7,23 +7,24 @@ import CustomInput from './components/custom-input/CustomInput';
 import Card from './components/card/Card';
 
 function App() {
+  const [arrayNum, setArrayNum] = React.useState(1);
+  const [randomBgColorArr, setRandomBgColorArr] = React.useState(['#ADD8E6'])
   const [name, setName] = React.useState("")
   const [surname, setSurname] = React.useState("")
   const [email, setEmail] = React.useState("")
   const [age, setAge] = React.useState("")
-  const [color, setColor] = React.useState('#000')
+  const [color, setColor] = React.useState('#000000')
   const [gender, setGender] = React.useState('')
   const [agree, setAgree] = React.useState("")
   const [error, setError] = React.useState("")
 
   const handleCancel = (e) => {
     e.preventDefault();
-    console.log("cancel clicked")
     setName("")
     setSurname("")
     setEmail("")
     setAge("")
-    setColor("#000")
+    setColor("#000000")
     setGender('')
     setGender("")
     setError("")
@@ -31,7 +32,11 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("submit clicked");
+    let nextNum = arrayNum + 1;
+    setArrayNum(nextNum)
+    let nextBgColor = getRandomColor()
+    let updatedBgColorArr = [...randomBgColorArr, nextBgColor]
+    setRandomBgColorArr(updatedBgColorArr)
   }
 
   const handleChange = (e) => {
@@ -81,81 +86,92 @@ function App() {
     }
   }
 
+  function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
   return (
     <div className="App">
-      <Card backgroundColor="lightblue">
-        <form onSubmit={handleSubmit}>
-          <Heading title="Card 1" textColor="#F00" />
-          <SubHeading title="first card description" textColor="#FFF" />
-          {error && <p className='error'>* {error}</p>}
-          <CustomInput
-            name='name'
-            type='text'
-            handleChange={handleChange}
-            value={name}
-            label='name'
-            placeholder="name"
-            required
-          />
-          <CustomInput
-            name='surname'
-            type='text'
-            handleChange={handleChange}
-            value={surname}
-            label='surname'
-            placeholder="surname"
-            required
-          />
-          <CustomInput
-            name='email'
-            type='email'
-            handleChange={handleChange}
-            value={email}
-            label='email'
-            placeholder="email"
-            required
-          />
-          <CustomInput
-            name='age'
-            type='text'
-            handleChange={handleChange}
-            value={age}
-            label='age'
-            placeholder="age"
-          />
-          <CustomInput
-            name='color'
-            type='color'
-            handleChange={handleChange}
-            value={color}
-            label='favorite color'
-            placeholder="favorite color"
-          />
+      {[...Array(arrayNum)].map((elementInArray, index) => ( 
+        <Card backgroundColor={randomBgColorArr[index]}>
+          <form onSubmit={handleSubmit}>
+            <Heading title="Card 1" textColor="#F00" />
+            <SubHeading title="first card description" textColor="#FFF" />
+            {error && <p className='error'>* {error}</p>}
+            <CustomInput
+              name='name'
+              type='text'
+              handleChange={handleChange}
+              value={name}
+              label='name'
+              placeholder="please enter your name"
+              required
+            />
+            <CustomInput
+              name='surname'
+              type='text'
+              handleChange={handleChange}
+              value={surname}
+              label='surname'
+              placeholder="please enter your surname"
+              required
+            />
+            <CustomInput
+              name='email'
+              type='email'
+              handleChange={handleChange}
+              value={email}
+              label='email'
+              placeholder="please enter your email"
+              required
+            />
+            <CustomInput
+              name='age'
+              type='text'
+              handleChange={handleChange}
+              value={age}
+              label='age'
+              placeholder="please enter your age"
+            />
+            <CustomInput
+              name='color'
+              type='color'
+              handleChange={handleChange}
+              value={color}
+              label='favorite color'
+              placeholder="please enter the favorite color"
+            />
 
-          <div className='radio-container'>
-            <input type="radio" value="male" id="male"
-              onChange={e => setGender(e.target.value)} name="gender" />
-            <label for="male">Male</label>
+            <div className='radio-container'>
+              <input type="radio" value="male" id="male"
+                onChange={e => setGender(e.target.value)} name="gender" />
+              <label for="male">Male</label>
 
-            <input type="radio" value="female" id="female"
-              onChange={e => setGender(e.target.value)} name="gender" />
-            <label for="female">Female</label>
-          </div>
+              <input type="radio" value="female" id="female"
+                onChange={e => setGender(e.target.value)} name="gender" />
+              <label for="female">Female</label>
+            </div>
 
-          <label>
-            <input
-              name='agree'
-              type="checkbox"
-              defaultChecked={agree}  // React checkbox onChange get value
-              onChange={e => setAgree(e.target.value)}  // React checkbox onChange setState 
-            />Would you like to receive notifications?
-          </label>
-          <div className='bottom-container'>
-            <Button text="Cancel" backgroundColor="#F00" textColor="#FFF" padding="15px" onClick={handleCancel} />
-            <Button text="Submit" backgroundColor="#0F0" textColor="#FF0" padding="15px" disabled={!name || !surname || !email || !age || !color || !gender} onClick={handleSubmit} />
-          </div>
-        </form>
-      </Card>
+            <label>
+              <input
+                name='agree'
+                type="checkbox"
+                defaultChecked={agree}  // React checkbox onChange get value
+                onChange={e => setAgree(e.target.value)}  // React checkbox onChange setState 
+              />Would you like to receive notifications?
+            </label>
+            <div className='bottom-container'>
+              <Button text="Cancel" backgroundColor="#F00" textColor="#FFF" padding="15px" onClick={handleCancel} />
+              <Button text="Submit" backgroundColor="#0F0" textColor="#FF0" padding="15px" disabled={!name || !surname || !email || !age || !color || !gender} onClick={handleSubmit} />
+            </div>
+          </form>
+        </Card>
+      ))}
     </div>
   );
 }
